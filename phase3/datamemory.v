8af4bd4f16d33cd5e6f059a1caa29b85731f3431
Sonia Abtahi <70149061+soniaabtahi1959@users.noreply.github.com>
2021-06-24 13:38:44 +0430

    Add files via upload

---
+++ phase3/datamemory.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:34:26 06/21/2021 
+// Design Name: 
+// Module Name:    datamemory 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module datamemory(address,datawrite,clk,memwrite,memread,readdata,memtoreg);
+input clk;
+input [15:0] address;
+input memtoreg;
+input memwrite;
+input memread;
+input [15:0] datawrite;
+output reg[15:0] readdata;
+reg [99:0] mem [15:0];	 
+always @*
+begin
+if(memwrite==1)
+ mem[address]=datawrite;
+if(memread==1)
+  begin 
+  readdata=mem[address];
+  end
+if(memtoreg==0)
+   readdata=address;
+else
+   readdata=mem[address];	
+end  
+endmodule
